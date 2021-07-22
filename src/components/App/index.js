878e80df1477d3e7a1fef88385fc53048472f1a0
Yannick-Beau <beauyannick@gmail.com>
2021-07-22 10:13:40 +0200

    components structure

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,13 +2,14 @@
 import React from 'react';
 
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import SearchBar from 'src/components/SearchBar';
+import Message from 'src/components/Message';
+import ReposResults from 'src/components/ReposResults';
+import './styles.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
     <h1>Composant : App</h1>
   </div>
 );
